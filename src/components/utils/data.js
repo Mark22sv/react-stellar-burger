545beb156a200040dac0e96b037f6019f6d1fa01
Mark22sv <116151585+Mark22sv@users.noreply.github.com>
2023-04-29 07:51:36 +0600

    fix revue2 убрал дублирование объекта PropTyps из utils step1

--- src/components/utils/data.js
+++ src/components/utils/data.js
@@ -213,12 +213,3 @@ export const data = [
   }
 ]
 
-export const ingredientPropTypes = PropTypes.shape({
-  "name": PropTypes.string.isRequired,
-  "type": PropTypes.string.isRequired,
-  "proteins": PropTypes.number.isRequired,
-  "fat": PropTypes.number.isRequired,
-  "carbohydrates": PropTypes.number.isRequired,
-  "calories": PropTypes.number.isRequired,
-  "price": PropTypes.number.isRequired,
-})
